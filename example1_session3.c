7110b7798f803ba8732efc29b77afcd89568f470
Melgazar <melgazar725@gmail.com>
2023-02-25 18:28:42 +0200

    Update example1_session3.c

--- example1_session3.c
+++ example1_session3.c
@@ -2,21 +2,28 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+
+// main 
 int main(void) {
 
+        // Setting Pins of PortA dirction to  ===> 0b00000001 ( Pin0(A0) = OutPut & rest of Pins as Input )
         DDRA = 0x01;
-        
+        // Setting Pins Value (1 or 0)  ===> 0b00000001 (Pin0(A0) = High( 5V ) & rest of Pins as 0V)
         PORTA = 0x01 ;
     
+        // infinte while Loop 
     while (1) {
-        
+            
+                 // Setting Pins Value (1 or 0)  ===> 0b00000000 (Pin0(A0) = LOW( 0V ) & rest of Pins as 0V)
                 PORTA = 0x00 ;
-                 
-                _delay_ms(1000); // => 1 s
-                
+         
+                _delay_ms(1000); //    Wait for 1000 MiliSecounds = 1 S
+            
+                 // Setting Pins Value (1 or 0)  ===> 0b00000001 (Pin0(A0) = High( 5V ) & rest of Pins as 0V)
                   PORTA = 0x01 ;
                   
-                _delay_ms(1000);
+                _delay_ms(1000); //    Wait for 1000 MiliSecounds = 1 S
                 
-    }
-}
+    } // End While
+        
+}// end main 
